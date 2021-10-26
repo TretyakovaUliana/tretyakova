1b7f1d0af7bf145ec1101b93e0ec0f97da58aa29
TretyakovaUliana <91208784+TretyakovaUliana@users.noreply.github.com>
2021-10-27 00:29:00 +0300

    Правка задачи 2

--- task2.R
+++ task2.R
@@ -4,7 +4,7 @@ library(dplyr)
 data = read_csv(file = "greendb.csv")
 
 data = data %>% filter(species_ru == "Береза пушистая")
-model = lm(height_m ~ adm_region, data)
+model = lm(d_trunk_m ~ adm_region, data)
 
 anova(model)
-# value 21, это больше 1 значит район влияет на среднюю высоту березы
+# value 17, это больше 1 значит район влияет на средний диаметр
