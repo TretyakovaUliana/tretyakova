06356743405110bd4aefb648067cee5ebfb6bb9e
TretyakovaUliana <91208784+TretyakovaUliana@users.noreply.github.com>
2021-11-02 22:45:08 +0300

    Update task3.R

--- task3.R
+++ task3.R
@@ -5,7 +5,7 @@ library(jsonlite)
 library(sf)
 
 
-greendb = read_csv("~/Downloads/ulya/greendb.csv")
+greendb = read_csv("~/urban_modeling/greendb.csv")
 
 greendb %>% summary
 greendb |> summary()
@@ -15,10 +15,23 @@ greendb |> summary()
 ### для послеления Десеновское вида береза пушистая
 ######
 
-fit_data = greendb %>% 
-  filter(species_ru == "Береза пушистая", adm_region == "поселение Десёновское", height_m < 100) %>% select(
+data = greendb %>% 
+  filter(species_ru == "Береза пушистая", adm_region == "поселение Десёновское") %>% select(
     height_m, d_trunk_m
   )
-ggplot(fit_data, aes(x=height_m, y=d_trunk_m)) +
-  geom_point()+
-  geom_smooth(method=lm)
+
+model = lm(d_trunk_m ~ height_m, data)
+
+summary(model)
+
+# d_trunk_m = 0.0115320 * heght_m +0.0421969
+# p < 0.001
+
+anova(model)
+# Analysis of Variance Table
+# Response: d_trunk_m
+# Df Sum Sq Mean Sq F value    Pr(>F)    
+# height_m    1 9.0661  9.0661  1048.2 < 2.2e-16 ***
+#   Residuals 726 6.2791  0.0086                      
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# Зависит p < 0.05
