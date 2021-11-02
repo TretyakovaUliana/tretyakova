53f87a52b66d2e3bd61bd0953005e0a9f06034cc
TretyakovaUliana <91208784+TretyakovaUliana@users.noreply.github.com>
2021-11-02 22:49:38 +0300

    Update task2.R

--- task2.R
+++ task2.R
@@ -1,10 +1,15 @@
 library(dplyr)
 
-
-data = read_csv(file = "greendb.csv")
-
-data = data %>% filter(species_ru == "Береза пушистая")
+data = greendb %>% filter(species_ru == "Береза пушистая")
+greendb = read_csv("~/urban_modeling/greendb.csv")
 model = lm(d_trunk_m ~ adm_region, data)
 
 anova(model)
-# value 17, это больше 1 значит район влияет на средний диаметр
+# Analysis of Variance Table
+# 
+# Response: d_trunk_m
+# Df  Sum Sq  Mean Sq F value    Pr(>F)    
+# adm_region  105  23.366 0.222533    17.6 < 2.2e-16 ***
+#   Residuals  8851 111.912 0.012644                      
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
